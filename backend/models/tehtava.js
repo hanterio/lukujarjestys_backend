53648bac22092e082893286fe23189d46cb2a30a
Hannes Vieth <hannesvieth@gmail.com>
2025-03-12 10:04:49 +0200

    mongoose komponentti ei toiminut, toimiikohan nyt?

--- backend/models/tehtava.js
+++ backend/models/tehtava.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+  
 const tehtavaSchema = new mongoose.Schema({
     kuvaus: String,
     opettaja: String,
